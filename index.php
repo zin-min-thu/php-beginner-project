b1fa0a1b65021d3a88c90e0e483b68ba73f93168
tommy <tommy@visibleone.com>
2024-05-08 23:47:43 +0630

    make custom database connection

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 
 require 'function.php';
 
-require 'router.php';
+// require 'router.php';
 
-// dd($_SERVER);
+require 'Database.php';
+
+// connect to our MySQL database
+$db = new Database();
+
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+    echo "<li>".$post['title']."</li>";
+}
