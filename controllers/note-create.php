bfc7973e4ac83aa047f85d44950a0ce4ead0f3eb
KoZin <zinminthu.dev@gmail.com>
2024-05-20 21:39:14 +0630

    make custom validator

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,7 @@
 <?php
 
+require('Validator.php');
+
 $config = require 'config.php';
 
 $db = new Database($config['database'], $config['dbuser'], $config['dbpassword']);
@@ -10,11 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $errors = [];
 
-    if(strlen($_POST['body']) == 0) {
-        $errors['body'] = 'Body field is required.';
-    }
-
-    if(strlen($_POST['body']) > 100) {
+    if(! Validator::string($_POST['body'], 1, 100)) {
         $errors['body'] = 'Body must not be exceed more than 100 characters.';
     }
 
