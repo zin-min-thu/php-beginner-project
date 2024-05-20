473b837902ec6b709a7f417c80621f6ff84602a5
KoZin <zinminthu.dev@gmail.com>
2024-05-20 16:53:47 +0630

    store form data into db

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,30 @@
 <?php
 
+$config = require 'config.php';
+
+$db = new Database($config['database'], $config['dbuser'], $config['dbpassword']);
+
 $heading = 'Note Create';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    dd($_POST);
+
+    $errors = [];
+
+    if(strlen($_POST['body']) == 0) {
+        $errors['body'] = 'Body field is required.';
+    }
+
+    if(strlen($_POST['body']) > 100) {
+        $errors['body'] = 'Body must not be exceed more than 100 characters.';
+    }
+
+    if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
+
 }
 
 require 'views/note-create.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -7,7 +7,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <p><?= $note['body'] ?></p>
+            <p><?= htmlspecialchars($note['body']) ?></p>
         </div>
     </main>
 
