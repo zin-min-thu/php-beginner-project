463e4bd0c212b51384a19bf3195b013b72f72ecd
KoZin <zinminthu.dev@gmail.com>
2024-06-09 22:49:51 +0630

    make password hash

--- views/index.view.php
+++ views/index.view.php
@@ -6,7 +6,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            Hello welcome to my page
+            Hello <?= $_SESSION['user']['email'] ?? 'Guest' ?>, welcome to my page.
         </div>
     </main>
 
