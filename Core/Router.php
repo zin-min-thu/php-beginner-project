eeba54fbfd0c180c60d6995e91083267a9ef0cc6
KoZin <zinminthu.dev@gmail.com>
2024-06-09 13:37:53 +0630

    build custom middleware

--- Core/Router.php
+++ Core/Router.php
@@ -2,6 +2,10 @@
 
 namespace Core;
 
+use Core\Middleware\Auth;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
+
 class Router
 {
     protected $routes = [];
@@ -12,13 +16,16 @@ class Router
             'uri'        => $uri,
             'controller' => $controller,
             'method'     => $method,
+            'middleware' => null,
         ];
+
+        return $this;
 
     }
 
     public function get($uri, $controller)
     {
-        $this->add('GET', $uri, $controller);
+        return $this->add('GET', $uri, $controller);
     }
 
     public function post($uri, $controller)
@@ -32,23 +39,48 @@ class Router
 
     public function delete($uri, $controller)
     {
-        $this->add('DELETE', $uri, $controller);
+        return $this->add('DELETE', $uri, $controller);
     }
 
     public function patch($uri, $controller)
     {
-        $this->add('PATCH', $uri, $controller);
+        return $this->add('PATCH', $uri, $controller);
     }
 
     public function put($uri, $controller)
     {
-        $this->add('PUT', $uri, $controller);
+        return $this->add('PUT', $uri, $controller);
+    }
+
+    public function only($key)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+        return $this;
     }
 
     public function route($uri, $method)
     {
         foreach ($this->routes as $route) {
             if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                // check middleware
+                // good way
+                Middleware::resolve($route['middleware']);
+                // if($route['middleware']) {
+                //     $middleware = Middleware::MAP[$route['middleware']];
+
+                //     (new $middleware)->handle();
+                // }
+
+                // bad way
+                // if ($route['middleware'] === 'guest') {
+                //     (new Guest)->handle();
+                // }
+
+                // if ($route['middleware'] === 'auth') {
+                //     (new Auth)->handle();
+                // }
+
                 return require base_path($route['controller']);
             }
         }
--- routes.php
+++ routes.php
@@ -13,7 +13,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
 $router->get('/note', 'controllers/notes/show.php');
@@ -21,5 +21,5 @@ $router->get('/note/edit', 'controllers/notes/edit.php');
 $router->patch('/note', 'controllers/notes/update.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
