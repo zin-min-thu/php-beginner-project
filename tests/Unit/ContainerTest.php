1447e900569e90a3404a087605f164a0abf0ac5f
KoZin <zinminthu.dev@gmail.com>
2024-06-22 21:42:58 +0630

    unit test and install packages

---
+++ tests/Unit/ContainerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Core\Container;
+
+test('it can resolve something out of the container', function() {
+    $container = new Container();
+
+    $container->bind('foo', fn() => 'bar');
+
+    $result = $container->resolve('foo');
+
+    expect($result)->toEqual('bar');
+
+});
